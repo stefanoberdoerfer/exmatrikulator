95b49427b515cd6d4f226a072ee73f6010cbeabd
Matthias Reichmann <matreich@uni-bremen.de>
2016-01-09 13:07:57 +0100

    Move font sizes and families to variables SASS (except FontAwesome)

--- src/main/gui/Gruntfile.js
+++ src/main/gui/Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
         },
         sass: {
             options: {
-                sourceMap: true
+                sourceMap: false
             },
             dist: {
                 files: {
