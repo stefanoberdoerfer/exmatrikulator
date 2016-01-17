5398dd7e3a24c2acf8645134fc7f6511058da2d8
Matthias Reichmann <matreich@uni-bremen.de>
2016-01-17 11:31:20 +0100

    NavigationController basics

---
+++ src/main/java/de/unibremen/opensores/controller/NavigationController.java
@@ -0,0 +1,45 @@
+package de.unibremen.opensores.controller;
+
+import java.util.*;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class NavigationController {
+    private Locale userLocale = Locale.GERMAN;
+
+    private static Map<String, Locale> languages;
+    static {
+        languages = new LinkedHashMap<> ();
+        languages.put("Deutsch", Locale.GERMAN);
+        languages.put("English", Locale.ENGLISH);
+    }
+
+    public List<String> getLanguages() {
+        return new ArrayList<>(languages.keySet());
+    }
+
+    public Locale getUserLocale() {
+        return userLocale;
+    }
+
+    public void setUserLocale(String l) {
+        System.out.println("Language setting to " + l);
+        Locale lang = languages.get(l);
+
+        if (lang == null) {
+            return;
+        }
+
+        FacesContext
+                .getCurrentInstance()
+                .getViewRoot()
+                .setLocale(lang);
+
+        this.userLocale = lang;
+        System.out.println("Language set to " + l);
+    }
+}
