2056a1c20a2b6f9d023c3fb2f1d765e4c0ea7f4a
Matthias Reichmann <matreich@uni-bremen.de>
2016-01-19 15:24:43 +0100

    Create course wizard: gui for each step except modals

---
+++ src/main/java/de/unibremen/opensores/controller/CourseCreateController.java
@@ -0,0 +1,12 @@
+package de.unibremen.opensores.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+
+/**
+ * Created by matthias on 19.01.16.
+ */
+@ManagedBean
+@RequestScoped
+public class CourseCreateController {
+}
