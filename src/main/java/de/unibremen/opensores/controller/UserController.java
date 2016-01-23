94213d45a24c6957759032f69bc7684c2bfc33a9
Matthias Reichmann <matreich@uni-bremen.de>
2016-01-23 21:18:57 +0100

    Modals and alerts for course creation
    Add profile.xhtml view, wich displays profiel information.
    
    Add editprofile.xhtml view, which is used to edit profile information.
    
    Add link to profile.xhtml view to base.xhtml.
    
    Add localisations needed for profile views.
    
    Remove excess whitespace from editprofile.xhtml.
    
    Edit saveUser() mehrod in UserController to return "profile.xhtml".

--- src/main/java/de/unibremen/opensores/controller/UserController.java
+++ src/main/java/de/unibremen/opensores/controller/UserController.java
@@ -50,8 +50,9 @@ public class UserController {
      * Saves the current user in the database.
      * @Pre The user has valid fields.
      */
-    public void saveUser() {
+    public String saveUser() {
         userService.update(user);
+        return "profile.xhtml";
     }
 
     /**
