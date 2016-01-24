1285cfc4b9dca33a7f081aaf5e921bc80612e5c5
Sören Tempel <tempel@uni-bremen.de>
2016-01-25 00:34:56 +0100

    JavaDoc for NavigationController

--- src/main/java/de/unibremen/opensores/controller/NavigationController.java
+++ src/main/java/de/unibremen/opensores/controller/NavigationController.java
@@ -17,22 +17,33 @@ import java.util.Map;
 @ManagedBean(name = "navigation")
 @SessionScoped
 public class NavigationController {
+    /**
+     * Current user locale.
+     */
     private Locale userLocale;
 
+    /**
+     * UserService instance.
+     */
     @EJB
     private UserService userService;
 
+    /**
+     * Map containing supported locales.
+     */
     private static Map<String, Locale> languages;
 
+    /**
+     * @todo This is retarded, locales should be fetched from faces-config.xml.
+     */
     static {
         languages = new LinkedHashMap<>();
         languages.put("Deutsch", Locale.GERMAN);
         languages.put("English", Locale.ENGLISH);
     }
 
-
     /**
-     * @todo.
+     * Creates a new NavigationController instance.
      */
     public NavigationController() {
         this.userLocale = FacesContext
@@ -41,57 +52,80 @@ public class NavigationController {
                 .getLocale();
     }
 
+    /**
+     * Returns a list of supported locales.
+     *
+     * @return List of supported locales.
+     */
     public List<String> getLanguages() {
         return new ArrayList<>(languages.keySet());
     }
 
+    /**
+     * Returns the locale for the current user.
+     *
+     * @return Locale used by the current user.
+     */
     public Locale getUserLocale() {
         return userLocale;
     }
 
     /**
-     * @todo.
-     * @param locale .
+     * Changes the locale for the logged in user.
+     *
+     * @param locale Locale to use.
      */
     public void setUserLocale(String locale) {
-        System.out.println("Language setting to " + locale);
         Locale lang = languages.get(locale);
-
         if (lang == null) {
             return;
         }
 
-        FacesContext
-                .getCurrentInstance()
+        FacesContext.getCurrentInstance()
                 .getViewRoot()
                 .setLocale(lang);
 
         this.userLocale = lang;
-        System.out.println("Language set to " + locale);
     }
 
     /**
-     * @todo.
-     * @return.
+     * Returns the user that is currently logged in.
+     *
+     * @return Currently logged in user.
      */
     public User getUser() {
         return (User)FacesContext
-                .getCurrentInstance()
-                .getExternalContext()
-                .getSessionMap()
-                .get("user");
+            .getCurrentInstance()
+            .getExternalContext()
+            .getSessionMap()
+            .get("user");
     }
 
+    /**
+     * Whether the given FacesMessage is an error message.
+     *
+     * @return True if it is, false otherwise.
+     */
     public boolean isError(FacesMessage message) {
-        return  message.getSeverity().equals(FacesMessage.SEVERITY_ERROR)
+        return message.getSeverity().equals(FacesMessage.SEVERITY_ERROR)
                 || message.getSeverity().equals(FacesMessage.SEVERITY_FATAL);
     }
 
+    /**
+     * Whether the given FacesMessage is a warning message.
+     *
+     * @return True if it is, false otherwise.
+     */
     public boolean isWarning(FacesMessage message) {
-        return  message.getSeverity().equals(FacesMessage.SEVERITY_WARN);
+        return message.getSeverity().equals(FacesMessage.SEVERITY_WARN);
     }
 
+    /**
+     * Whether the given FacesMessage is a success message.
+     *
+     * @return True if it is, false otherwise.
+     */
     public boolean isSuccess(FacesMessage message) {
-        return  message.getSeverity().equals(FacesMessage.SEVERITY_INFO);
+        return message.getSeverity().equals(FacesMessage.SEVERITY_INFO);
     }
 }
