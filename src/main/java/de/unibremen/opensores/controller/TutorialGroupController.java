1e31abdf794ca24de8a743ba02a44758db4b6788
Sören Tempel <tempel@uni-bremen.de>
2016-02-05 21:13:20 +0100

    Initial tutorial creation code

--- src/main/java/de/unibremen/opensores/controller/TutorialGroupController.java
+++ src/main/java/de/unibremen/opensores/controller/TutorialGroupController.java
@@ -1,12 +1,20 @@
 package de.unibremen.opensores.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import de.unibremen.opensores.model.User;
 import de.unibremen.opensores.model.Course;
 import de.unibremen.opensores.model.Tutorial;
+import de.unibremen.opensores.model.PrivilegedUser;
+import de.unibremen.opensores.service.UserService;
 import de.unibremen.opensores.service.CourseService;
 
 import java.util.List;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.io.Serializable;
+import java.text.MessageFormat;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
@@ -23,17 +31,43 @@ import javax.servlet.http.HttpServletRequest;
  */
 @ManagedBean
 @ViewScoped
-public class TutorialGroupController {
+public class TutorialGroupController implements Serializable {
+    /**
+     * Unique serial version uid.
+     */
+    private static final long serialVersionUID = -626531390355925094L;
+
+    /**
+     * The log4j logger.
+     */
+    private Logger log = LogManager.getLogger(TutorialGroupController.class);
+
     /**
      * The user service for connection to the database.
      */
     @EJB
-    private CourseService courseService;
+    private transient UserService userService;
+
+    /**
+     * The course service for connection to the database.
+     */
+    @EJB
+    private transient CourseService courseService;
+
+    /**
+     * Name for a new tutorial.
+     */
+    public String createTutorialName;
+
+    /**
+     * Comma seperated string of tutor emails;
+     */
+    public String createTutorialTutors;
 
     /**
      * Course for this tutorial.
      */
-    private Course course;
+    private transient Course course;
 
     /**
      * Method called on bean initialization.
@@ -73,6 +107,61 @@ public class TutorialGroupController {
     }
 
     /**
+     * Creates a new tutorial.
+     */
+    public void createTutorial() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ResourceBundle bundle = ResourceBundle.getBundle("messages",
+            facesContext.getViewRoot().getLocale());
+
+        if (course == null) {
+            String msg = bundle.getString("courses.fail");
+            facesContext.addMessage(null, new FacesMessage(FacesMessage
+                .SEVERITY_FATAL, bundle.getString("common.error"), msg));
+            return;
+        }
+
+        Tutorial tutorial = new Tutorial();
+        tutorial.setCourse(course);
+        tutorial.setName(createTutorialName);
+
+        List<PrivilegedUser> tutors = new ArrayList<>();
+        String[] emails = createTutorialTutors.split(",");
+        for (String email : emails) {
+            email = email.trim();
+            User user = userService.findByEmail(email);
+            if (user == null) {
+                String fmt = bundle.getString("common.studentDoesNotExist");
+                String msg = new MessageFormat(fmt).format(new Object[]{email});
+
+                facesContext.addMessage(null, new FacesMessage(FacesMessage
+                    .SEVERITY_FATAL, bundle.getString("common.error"), msg));
+                return;
+            }
+
+            PrivilegedUser tutor = courseService.findTutor(course, user);
+            if (tutor == null) {
+                log.debug("Made user with email " + user.getEmail() + " a tutor");
+                tutor = new PrivilegedUser();
+
+                tutor.setSecretary(false);
+                tutor.setCourse(course);
+                tutor.setUser(user);
+                tutor.setHidden(false);
+            }
+
+            tutor.getTutorials().add(tutorial);
+            tutorial.getTutors().add(tutor);
+            course.getTutors().add(tutor);
+        }
+
+        course.getTutorials().add(tutorial);
+        course = courseService.update(course);
+
+        log.debug("Created new tutorial " + tutorial.getName());
+    }
+
+    /**
      * Returns a list of all tutorials for this course.
      *
      * @return List of tutorials or null if non exist.
@@ -97,5 +186,41 @@ public class TutorialGroupController {
      */
     public Course getCourse() {
         return this.course;
+    }
+
+    /**
+     * Sets the name for a new tutorial.
+     *
+     * @param createTutorialName Tutorial name.
+     */
+    public void setCreateTutorialName(String createTutorialName) {
+        this.createTutorialName = createTutorialName;
+    }
+
+    /**
+     * Returns the name for a new tutorial.
+     *
+     * @return Name for a new tutorial.
+     */
+    public String getCreateTutorialName() {
+        return createTutorialName;
+    }
+
+    /**
+     * Sets the tutors for a new tutorial.
+     *
+     * @param createTutorialTutors Tutor name.
+     */
+    public void setCreateTutorialTutors(String createTutorialTutors) {
+        this.createTutorialTutors = createTutorialTutors;
+    }
+
+    /**
+     * Returns the tutors for a new tutorial.
+     *
+     * @return Comma seperated string of emails.
+     */
+    public String getCreateTutorialTutors() {
+        return createTutorialTutors;
     }
 }
--- src/main/java/de/unibremen/opensores/service/CourseService.java
+++ src/main/java/de/unibremen/opensores/service/CourseService.java
@@ -1,5 +1,7 @@
 package de.unibremen.opensores.service;
 
+import de.unibremen.opensores.model.User;
+import de.unibremen.opensores.model.PrivilegedUser;
 import de.unibremen.opensores.model.Course;
 
 import javax.ejb.Stateless;
@@ -13,6 +15,27 @@ import java.util.List;
  */
 @Stateless
 public class CourseService extends GenericService<Course> {
+
+    /**
+     * Find tutor of this course using a user object.
+     *
+     * @param course Course to look at.
+     * @param user User associated with this tutor.
+     * @return Tutor with this email or null.
+     */
+    public PrivilegedUser findTutor(Course course, User user) {
+        List<PrivilegedUser> privUsers = em.createQuery(
+                "SELECT DISTINCT u FROM PrivilegedUser u"
+                + " JOIN u.course AS c"
+                + " WITH c.courseId = :courseId"
+                + " WHERE u.isSecretary = false"
+                + " AND u.user.userId = :userId", PrivilegedUser.class)
+            .setParameter("courseId", course.getCourseId())
+            .setParameter("userId", user.getUserId())
+            .getResultList();
+
+        return (privUsers.isEmpty()) ? null : privUsers.get(0);
+    }
 
     /**
      * Lists all courses of the COURSES table.
