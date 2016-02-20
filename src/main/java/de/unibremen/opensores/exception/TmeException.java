3430e88b7587929d925634cc99787a311d834fb3
Sören Tempel <tempel@uni-bremen.de>
2016-02-20 16:04:00 +0100

    Implement basic TME import view in /admin
    So far only new users can be imported and the frontend view is pretty
    ugly currently. Apart from that it works as expected and could be merged
    into the master as a first step.
    
    See #66

---
+++ src/main/java/de/unibremen/opensores/exception/TmeException.java
@@ -0,0 +1,10 @@
+package de.unibremen.opensores.exception;
+
+/**
+ * Exception for mailformated TME files.
+ */
+public class TmeException extends Exception {
+    public TmeException(final String msg) {
+        super(msg);
+    }
+}
