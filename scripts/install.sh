e63f030c797764f9de70aac5c1a487240eb2136f
Lorenz Hüther <lorenz1@uni-bremen.de>
2016-03-04 15:50:42 +0100

    Fixed typo in install.sh, that prevented successful deployment

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,5 +9,5 @@ bin/asadmin create-jdbc-connection-pool \
         --datasourceclassname "org.apache.derby.jdbc.ClientDataSource" \
         --restype "javax.sql.DataSource" \
         --property User=exmatrikulator:Password=$1:ConnectionAttributes=\;create\\=true:dataBaseName=Exmatrikulator:ServerName=localhost:PortNumber=1527 exmatrikulator
-bin/asadmin create-jdbc-resource --connectionpoolid exmatrikulator jdbc/exmatriku2lator
+bin/asadmin create-jdbc-resource --connectionpoolid exmatrikulator jdbc/exmatrikulator
 bin/asadmin deploy exmatrikulator.war
